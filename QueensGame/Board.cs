6296ada04e569cf9d2726f3ad028aaca5e32aad2
bellevin <bellevin@uqtr.ca>
2021-04-02 17:01:04 -0400

    Some sort of something

---
+++ QueensGame/Board.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace QueensGame
+{
+    class Board
+    {
+        public int[,] data;
+        private int size;
+
+        public Board(int size)
+        {
+            this.size = size;
+        }
+
+        public void InitializeData()
+        {
+            data = new int[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    data[i, j] = 0;
+        }
+
+        private bool PositionAvailable(int i, int j)
+        {
+            if (!VerifyUpLine(i, j - 1)) { return false; }
+            if (!VerifyUpRightLine(i+1, j-1)) { return false; }
+            if (!VerifyRightLine(i+1, j)) { return false; }
+            if (!VerifyDownRightLine(i+1, j+1)) { return false; }
+            if (!VerifyDownLine(i, j+1)) { return false; }
+            if (!VerifyDownLeftLine(i-1, j+1)) { return false; }
+            if (!VerifyLeftLine(i-1, j)) { return false; }
+            if (!VerifyUpLeftLine(i-1, j-1)) { return false; }
+
+            return true;
+        }
+
+        public List<int[,]> FindQueensLocations(bool allSolutions)
+        {
+            List<int[,]> solutions = new List<int[,]>();
+
+            for (int i = 0; i < size; i++)
+            {
+                InitializeData();
+
+                List<Point> queensPositions = new List<Point>();
+
+                data[0, i] = 1;
+                queensPositions.Add(new Point(0, i));
+
+                Point lastPoint = new Point(0, i);
+                Point nextLocation;
+                while (queensPositions.Count < size)
+                {
+                    nextLocation = new Point(lastPoint.X + 1, lastPoint.Y + 2);
+                    MakeCoordinatesSure(ref nextLocation);
+
+                    if (PositionAvailable(nextLocation.X, nextLocation.Y))
+                    {
+                        queensPositions.Add(nextLocation);
+                        data[nextLocation.X, nextLocation.Y] = 1;
+                        lastPoint = nextLocation;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error with location");
+                    }
+                }
+
+                solutions.Add(data);
+                if (!allSolutions)
+                    i = size;
+            }
+
+            return solutions;
+        }
+
+        private void MakeCoordinatesSure(ref Point nextLocation)
+        {
+            if(nextLocation.X >= size)
+            {
+                nextLocation.X = nextLocation.X - size;
+            }
+
+            if(nextLocation.Y >= size)
+            {
+                nextLocation.Y = nextLocation.Y - size;
+            }
+        }
+
+        private bool VerifyLeftLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyLeftLine(i - 1, j);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        private bool VerifyDownLeftLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyDownLeftLine(i - 1, j + 1);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        private bool VerifyDownLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyDownLine(i, j + 1);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        private bool VerifyDownRightLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyDownRightLine(i + 1, j + 1);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        private bool VerifyUpRightLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyUpRightLine(i + 1, j - 1);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        private bool VerifyRightLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyRightLine(i + 1, j);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+
+        private bool VerifyUpLeftLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyUpLeftLine(i-1, j-1);
+                else
+                    return false;
+            }else
+                return true;
+        }
+
+        private bool VerifyUpLine(int i, int j)
+        {
+            if (i < data.GetLength(0) && i >= 0 && j < data.GetLength(1) && j >= 0)
+            {
+                if (VerifyCell(i, j))
+                    return VerifyUpLine(i, j - 1);
+                else
+                    return false;
+            }
+            else
+                return true;
+        }
+        private bool VerifyCell(int i, int j)
+        {
+            return data[i, j] == 0;
+        }
+    }
+}
--- QueensGame/Program.cs
+++ QueensGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace QueensGame
 {
@@ -6,7 +7,46 @@ namespace QueensGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Queens' Game");
+            int size = 0;
+            do
+            {
+                Console.WriteLine("Please enter the size of the board");
+                try
+                {
+                    size = Int32.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Error - Size must be an integer - Error");
+                }
+            } while (true);
+
+            Console.WriteLine("Do you want all solutions (yes) or only one (no)");
+            bool allSolution = Console.ReadLine().ToLower().Equals("yes");
+            Console.WriteLine();
+                
+            Board b = new Board(size);
+
+            List<int[,]> solutions = b.FindQueensLocations(allSolution);
+
+            foreach(int[,] solution in solutions)
+            {
+                Console.WriteLine("Solution " + (solutions.IndexOf(solution) + 1));
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                        Console.Write(solution[i, j] + " ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
